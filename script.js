7d5af576ca0b01366e704417e71eac2761b30353
bitLoop <66575644+bitLoopLV@users.noreply.github.com>
2021-08-17 09:30:39 -0700

    Check is browser supports fullscreen first

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 function beginExam() {
   //var input = document.getElementById('inputBox').value;
   var examId = window.location.search.split('?examId=')[1];
-  if(examId && examId.length > 0) {
-    var examSrc = `https://docs.google.com/forms/d/e/${examId}/viewform?embedded=true`;
-    document.getElementById('examFrame').src = examSrc;
-    document.getElementById('exam').style.display = 'inline-block';
-    document.getElementById('inputForm').style.display = 'none';
-    document.getElementById('footer').style.display = 'none';
-    launchFullscreen(document.documentElement, endExam);
-    document.addEventListener('visibilitychange', e => {
-      endExam();
-    });
+  if(supportsFullscreen()) {
+    if(examId && examId.length > 0) {
+      var examSrc = `https://docs.google.com/forms/d/e/${examId}/viewform?embedded=true`;
+      document.getElementById('examFrame').src = examSrc;
+      document.getElementById('exam').style.display = 'inline-block';
+      document.getElementById('inputForm').style.display = 'none';
+      document.getElementById('footer').style.display = 'none';
+      launchFullscreen(document.documentElement, endExam);
+      document.addEventListener('visibilitychange', e => {
+        endExam();
+      });
+    }
+    else {
+      alert('Invalid exam ID, please check if the link has been correctly copied.')
+    }
   }
   else {
-    alert('Invalid exam ID, please check if the link has been correctly copied.')
+    alert('Unsupported browser, please ensure you are using Chrome or Safari on desktop');
   }
 }
 
@@ -23,7 +28,9 @@ function endExam() {
   document.getElementById('footer').style.display = '';
   document.getElementById('examFrame').contentWindow.location.reload();
 }
-
+function supportsFullscreen() {
+  return document.fullscreenEnabled || document.mozFullscreenEnabled ||  document.webkitFullscreenEnabled || document.msFullscreenEnabled;
+}
 function launchFullscreen(element, onExit) {
   if(element.requestFullscreen) {
     element.requestFullscreen();
